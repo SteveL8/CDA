c93338b8ac20dfe4ab02d26f3b9b3f2defb86ead
Steve <steve-ya@hotmail.com>
2024-11-14 15:29:37 +0100

    t

--- fil_rouge/villageGreen/src/Controller/PrincipalController.php
+++ fil_rouge/villageGreen/src/Controller/PrincipalController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Rubrique;
+use App\Entity\SousRubrique;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,4 +21,31 @@ class PrincipalController extends AbstractController
             'rubriques' => $rubriques,
         ]);
     }
+
+    #[Route('/rubrique/{id}/sous_rubriques', name: 'app_rubrique_sous')]
+public function showSousRubriques(int $id, EntityManagerInterface $entityManager): Response
+{
+    // Récupérer la rubrique avec ses sous-rubriques
+    $rubrique = $entityManager->getRepository(Rubrique::class)->find($id);
+
+    if (!$rubrique) {
+        throw $this->createNotFoundException('Rubrique non trouvée');
+    }
+
+    // Forcer le chargement des sous-rubriques et les passer en tableau pour éviter le lazy loading
+    $sousRubriques = $rubrique->getSousRubriques()->toArray(); // Cela garantit que la collection est initialisée
+
+    // Vérifier si sous-rubriques est bien rempli
+    if (empty($sousRubriques)) {
+        $sousRubriques = null;
+    }
+
+    return $this->render('principal/sous_rubriques.html.twig', [
+        'rubrique' => $rubrique,
+        'sousRubriques' => $sousRubriques,  // Passer la collection initialisée
+    ]);
+}
+
+    
+
 }
